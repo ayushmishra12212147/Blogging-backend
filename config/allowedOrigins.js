7ea025e28e937429e322d448e349045c81a11a04
Ayush Kumar Mishra <ayushkumar.12212147@lpu.in>
2024-07-24 00:19:47 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins=[
     'http://localhost:5173',
     'http://localhost:4200',
     'http://localhost:3001',
-    'https://blogging-with-react.netlify.app/'
+    'https://blogging-with-react.netlify.app'
 
 
 ];
